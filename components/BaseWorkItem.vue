6a8c3a94380f74e0625b5359664bc98a4e0899c7
khriztianmoreno <khriztianmoreno@gmail.com>
2018-01-06 23:18:29 -0500

    Add the work experiences data

---
+++ components/BaseWorkItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-xs-12">
+    <div class="item-block">
+      <header>
+        <img :src="work.logo" alt="Logo Company">
+        <div class="hgroup">
+          <h4>{{ work.company }}</h4>
+          <h5>{{ work.title }}</h5>
+        </div>
+        <h6 class="time">{{ work.years }}</h6>
+      </header>
+      <div class="item-body">
+        <p>Responsibilities:</p>
+        <ul>
+          <li v-for="(responsibility, key) in work.responsibilities" :key="key">
+            {{ responsibility }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['work']
+  }
+</script>

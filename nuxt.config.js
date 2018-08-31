d0ecdcb9d767bea6f13ebec18b050d83a022d3a5
Emmanuel Deossa <edhgt89@hotmail.com>
2018-08-30 23:52:05 -0500

    Fix typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'khriztianmoreno | FullStack Javascritp Developer',
+    title: 'khriztianmoreno | FullStack Javascript Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

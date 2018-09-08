9294111168200e0aec398447dd139d274c452482
khriztianmoreno <khriztianmoreno@gmail.com>
2018-09-08 09:27:21 -0500

    Add the emojis to title page

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'khriztianmoreno | FullStack Javascript Developer',
+    title: 'khriztianmoreno | FullStack Javascript Developer 👨🏼‍💻👨🏼‍🏫',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
